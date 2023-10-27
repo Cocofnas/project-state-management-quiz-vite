57b790b43968bcf2b7f9d18a0c6bf8b0a28a8577
Lisa Dahlkar <lisadahlkar@MacBook-Pro.local>
2023-10-27 10:47:41 +0200

    button styling

--- src/components/Restart.jsx
+++ src/components/Restart.jsx
@@ -1,6 +1,5 @@
 import "./buttons.css";
 
 export const Restart = ({ onRestart }) => {
-  return <div class="restart-button">
-    <button onClick={onRestart}>Restart Quiz</button></div>;
+  return <div className="restart"><button className="restart-button" onClick={onRestart}>Restart Quiz</button></div>;
 };
